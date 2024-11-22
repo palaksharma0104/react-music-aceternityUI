53e799b6ab779f22810c54f203e1dd3dbf8d36de
palaksharma0104 <palak16221@gmail.com>
2024-11-22 16:46:35 +0530

    commit

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import { Updock } from "next/font/google";
+import FeaturedCourses from "./components/FeaturedCourses";
+import HeroSection from "./components/HeroSection";
+import MusicSchoolTestimonials from "./components/TestimonialCards";
+import WhyChooseUs from "./components/WhyChooseUs";
+import UpcomingWebinars from "./components/UpcomingWebinars";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+    <HeroSection />
+    <FeaturedCourses />
+    <WhyChooseUs />
+    <MusicSchoolTestimonials />4
+    <UpcomingWebinars />
+    </main>
+  );
+}
